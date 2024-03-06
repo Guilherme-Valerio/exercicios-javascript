cba7968ef4c728de53ee545b54fc2c95d40daf0d
Guilherme da Silva Valerio <guilherme.svalerio@hotmail.com>
2024-03-06 11:00:18 -0300

    add exercises

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu veículo é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Voce passou da velocidade permitida, MULTADO!')
+}
+console.log('Use sempre sinto de segurança')
